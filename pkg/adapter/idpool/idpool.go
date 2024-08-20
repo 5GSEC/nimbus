750aba27aee6cf84d43fb49b8fb318eb00606b67
Anurag Rajawat <anurag@accuknox.com>
2024-08-20 23:48:30 +0530

    fix(adapter): Update intentID to assessTLS from ensureTLS
    Signed-off-by: Anurag Rajawat <anurag@accuknox.com>

--- pkg/adapter/idpool/idpool.go
+++ pkg/adapter/idpool/idpool.go
@@ -17,7 +17,7 @@ const (
 	DisallowCapabilities      = "disallowCapabilities"
 	ExploitPFA                = "preventExecutionFromTempOrLogsFolders"
 	CocoWorkload              = "cocoWorkload"
-	EnsureTLS                 = "ensureTLS"
+	AssessTLS                 = "assessTLS"
 	DenyENAccess              = "denyExternalNetworkAccess"
 )
 
@@ -49,7 +49,7 @@ var KyvIds = []string{
 
 // k8tlsIds are IDs supported by k8tls.
 var k8tlsIds = []string{
-	EnsureTLS,
+	AssessTLS,
 }
 
 // IsIdSupportedBy determines whether a given ID is supported by a security engine.
